1e3b3dffcc38b192b7c0312313077574f35fea1e
Stuart Kuredjian <108@holbertonschool.com>
2017-05-02 22:25:45 +0000

    try with double quotes

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,6 @@
 i = 0
 #for i in range(0, 26):#
 while i < 26:
-    if chr(97+i) != 'q' and chr(97+i) != 'e':
+    if chr(97+i) != "q" and chr(97+i) != "e":
         print(chr(97+i), end='')
     i = i + 1
