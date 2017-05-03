74c95aba5f9c1521c86456627d9dd05e8db76e7c
Stuart Kuredjian <108@holbertonschool.com>
2017-05-03 02:04:42 +0000

    refactor

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         if ord(c) >= 97 and ord(c) <= 122:
             print("{}".format(chr(ord(c) - 32)), end='')
         else:
-            print("{}".format(c), end="")
-    print("")
+            print(c, end='')
+    print()
