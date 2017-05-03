ef4f8c017c27cfd380a311077f35350c6defccfb
Stuart Kuredjian <108@holbertonschool.com>
2017-05-03 02:49:15 +0000

    refactor without putting if in print statement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end='')
-        else:
-            print(c, end='')
-    print("")
+        char = ord(c)
+        if char >= 97 and char <= 122:
+            char -= 32
+        print(chr(char), end='')
+    print()
