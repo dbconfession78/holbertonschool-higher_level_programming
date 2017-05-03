2ef4a8e8adb87119aaf3ae2ae1bf2965576adda4
Stuart Kuredjian <108@holbertonschool.com>
2017-05-03 00:06:09 +0000

    refactor

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(1+i, 10):
-            print(i, end='')
-            if i != 8:
-                print(j, end=', ')
-            else:
-                print(j)
+        if i == 8 and j == 9:
+            print(89)
+        else:
+            print("{:d}{:d}".format(i, j), end=', ')
