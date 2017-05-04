ed0aa7409fa4e12edd25a1e906635d7b11be270c
Stuart Kuredjian <108@holbertonschool.com>
2017-05-04 21:13:32 +0000

    2-args.py: initial commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    if argc == 1:
+        print("0 argument.")
+    elif argc == 2:
+        print("1 argument:\n1: {:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(argc-1))
+        for i in range(1, argc):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
