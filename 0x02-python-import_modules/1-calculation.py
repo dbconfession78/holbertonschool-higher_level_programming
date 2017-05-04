b15ff54ab3074ec64e166fc665e209d3c03c2253
Stuart Kuredjian <108@holbertonschool.com>
2017-05-04 20:39:52 +0000

    add integer format specifiers

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ import calculator_1
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
