bdadf875c0821634af0e1c5ebd240922df783955
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 21:25:02 +0000

    fix error where original list idx was being replaced

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,7 @@ def new_in_list(my_list, idx, element):
     if my_list is None:
         return
     if idx > len(my_list) - 1 or idx < 0:
-        return my_list
-    my_list[idx] = element
-    return my_list.copy()
+        return my_list.copy()
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
