419e8133b7aae6523488a849597806b31a4ac2b3
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 19:20:28 +0000

    6-print_matrix_integer.py: initial commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+DEFAULT = object()
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) == 0 and len(matrix) == 1:
+        print()
+        return
+
+    for i in range(0, len(matrix)):
+        print("{}".format(matrix[i]))
