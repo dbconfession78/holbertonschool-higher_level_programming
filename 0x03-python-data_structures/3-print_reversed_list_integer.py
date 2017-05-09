86049bbb76bd99b55cf81d651a3a4b7d91e512f5
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 20:36:43 +0000

    add condition for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
     reverse_my_list = reversed(my_list)
     for i in reverse_my_list:
         print("{:d}".format(i))
