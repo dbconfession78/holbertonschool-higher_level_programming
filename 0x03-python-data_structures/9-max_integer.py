3837a7389cae31709f4e3a7af92410fd1683da56
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 23:53:55 +0000

    add condition for element being equal to the first element

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     _max = 0
-    if my_list is None:
+    if my_list is None or len(my_list) == 0:
         return None
 
     for elem in my_list:
-        if elem > _max:
+        if elem > _max or elem == my_list[0]:
             _max = elem
 
     return _max
