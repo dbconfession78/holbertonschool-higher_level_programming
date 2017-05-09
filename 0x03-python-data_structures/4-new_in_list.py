c5075a73afed71044abd8ad9ae551da3dad35233
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 20:46:29 +0000

    1. add condition for my_list is None 2. add condtition for less than 0

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > len(my_list) - 1:
+    if my_list is None:
+        return
+    if idx > len(my_list) - 1 or idx < 0:
         return my_list.copy()
     my_list[idx] = element
     return my_list.copy()
