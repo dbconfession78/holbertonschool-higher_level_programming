86e27ef85e6eaae3ade7d43ceca21264597af881
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 20:59:23 +0000

    return initial list on fail instead of copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if my_list is None:
         return
     if idx > len(my_list) - 1 or idx < 0:
-        return my_list.copy()
+        return my_list
     my_list[idx] = element
     return my_list.copy()
