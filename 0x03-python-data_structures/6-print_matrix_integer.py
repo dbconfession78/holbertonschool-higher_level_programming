732d060f6d877c87fbca800864d7c99cbbfe09c8
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 21:49:56 +0000

    add condition for no param passed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 DEFAULT = object()
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
+
     if len(matrix[0]) == 0 and len(matrix) == 1:
         print()
         return
 
     for i in range(0, len(matrix)):
-        print("{:d}".format(matrix[i]))
+        print("{}".format(matrix[i]))
