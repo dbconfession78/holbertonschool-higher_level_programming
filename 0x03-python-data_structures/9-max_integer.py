3c7523e689ed3a814576218a0ce766704ca9f4d9
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 23:48:55 +0000

    9-max_integer.py: initial commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    _max = 0
+    if my_list is None or len(my_list) == 0:
+        return None
+
+    for elem in my_list:
+        if elem > _max:
+            _max = elem
+
+    return _max
