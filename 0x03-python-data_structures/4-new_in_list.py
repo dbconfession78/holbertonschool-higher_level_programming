4b3c10985cb3e44039075daf6798864137f0d5e1
Stuart Kuredjian <108@holbertonschool.com>
2017-05-09 20:44:35 +0000

    fix condition for out of bounds index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return my_list.copy()
     my_list[idx] = element
     return my_list.copy()
