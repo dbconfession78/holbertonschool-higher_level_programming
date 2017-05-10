9ff1e1ff872f68b3c0fa8385a13291066d33a6e0
Stuart Kuredjian <108@holbertonschool.com>
2017-05-10 00:33:08 +0000

    move _max initialization after empty list check

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    _max = my_list[0]
+    _max = 0
+
     if my_list is None or len(my_list) == 0:
         return None
+
+    _max = my_list[0]
 
     for elem in my_list:
         if elem > _max:
