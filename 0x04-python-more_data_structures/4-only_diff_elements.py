b7871a0f927709689db18fa0824c53789b31efc5
Stuart Kuredjian <108@holbertonschool.com>
2017-05-11 22:07:02 +0000

    PASSED: asdd None condition handler

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,6 +2,8 @@
 def only_diff_elements(set_1, set_2):
     result_set = set()
 
+    if set_1 is None or set_2 is None:
+        return
     for elem in set_1:
         if elem not in set_2:
             result_set.add(elem)
