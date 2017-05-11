b5f3173d55c8896a75022af17bed53af59e57778
Stuart Kuredjian <108@holbertonschool.com>
2017-05-11 22:40:43 +0000

    PASSED: add None handling

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,8 @@
 def multiply_by_2(my_dict):
     result_dict = {}
 
+    if my_dict is None:
+        return
     for key in my_dict:
         result_dict[key] = my_dict[key] * 2
     return result_dict
