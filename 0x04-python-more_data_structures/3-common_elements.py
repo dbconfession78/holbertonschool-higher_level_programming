c6521ceb09b77a80bdaf9fa6071a0437d7c6ff6c
Stuart Kuredjian <108@holbertonschool.com>
2017-05-11 21:49:27 +0000

    PASSED: add None condition handler

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     common_elements = set()
+
+    if set_1 is None or set_2 is None:
+        return
     for elem in set_1:
         if elem in set_2:
             common_elements.add(elem)
