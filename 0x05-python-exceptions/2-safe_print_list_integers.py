78cb5ebb26ba4b570c2c995f897b1fe1cb33a6af
Stuart Kuredjian <108@holbertonschool.com>
2017-05-12 20:31:35 +0000

    add exceptions for ValueError and TypeError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,12 @@ def safe_print_list_integers(my_list=[], x=0):
     int_count = 0
     for i in range(x):
         try:
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end='')
-                int_count = int_count + 1
+            print("{:d}".format(my_list[i]), end='')
+            int_count = int_count + 1
         except IndexError as err:
-            print(err)
             raise
+        except (ValueError, TypeError) as err:
+            Exception
 
     print()
     return int_count
