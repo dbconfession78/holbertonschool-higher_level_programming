9e45f82703b8b6a6d2909a17b2ba54efec9bb46b
Stuart Kuredjian <108@holbertonschool.com>
2017-05-12 22:24:11 +0000

    move return of True to else condtion of try statement

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,8 +9,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except ValueError as err:
-        # print("Exception:{}".format(err), file=sys.stderr)
         sys.stderr.write("Exception: {}\n".format(err))
         return False
+    else:
+        return True
