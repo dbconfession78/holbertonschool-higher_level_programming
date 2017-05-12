5bc7615bdef4b7fa753faca828600ec1e6989f2c
Stuart Kuredjian <108@holbertonschool.com>
2017-05-12 21:37:25 +0000

    6-raise_exception.py: initial commit

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# raise_exception_msg - raises a name exception with a message
+# message: the message to output on NameError
+# Return: no return
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
