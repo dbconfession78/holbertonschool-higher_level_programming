09c1f1356785261d270c2120f79117436efe8029
Stuart Kuredjian <108@holbertonschool.com>
2017-05-12 00:39:44 +0000

    1. add value not in dict handler 2. refactor

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def complex_delete(my_dict, value):
-    i = 0
+    del_keys = []
 
-    while i < len(my_dict):
-        for idx, (key, val) in enumerate(my_dict.items()):
-            if val == value:
-                my_dict.pop(key)
-                if idx == len(my_dict) - 1:
-                    return my_dict
-                else:
-                    i = 0
-                    break
-            i = i + 1
+    if my_dict is None or value is None:
+        return
+    if value not in my_dict.values() or len(my_dict) == 0:
+        return my_dict
+    for (key, val) in my_dict.items():
+        if val == value:
+            del_keys.append(key)
+    for key in del_keys:
+        del(my_dict[key])
     return my_dict
