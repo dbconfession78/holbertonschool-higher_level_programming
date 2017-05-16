6e69a033c61e29777bf09995b1983d663800925f
Stuart Kuredjian <108@holbertonschool.com>
2017-05-16 22:01:46 +0000

    fix missing colon

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -14,7 +14,7 @@ def print_square(size):
     Raises a ValueError if 'size' is less than 0
     """
 
-    if not isinstance(size, int)
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
