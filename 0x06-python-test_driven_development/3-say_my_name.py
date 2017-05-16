516af9b4c202d11292e9b631c9bc28b3abe0a35e
Stuart Kuredjian <108@holbertonschool.com>
2017-05-16 21:29:21 +0000

    correct spelling error in function documentation

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -9,8 +9,8 @@ import sys
 def say_my_name(first_name, last_name=""):
     """
     say_my_name: prints "My name is " followed by a first and last name.
-    Raises a TypeError If either first or last name are not stringd.
-    Raises a TypeError ig there isn't at lesst one string provided
+    Raises a TypeError if either first or last name are not stringd.
+    Raises a TypeError ii there isn't at lesst one string provided
     """
 
     if not isinstance(first_name, str) or first_name is None:
