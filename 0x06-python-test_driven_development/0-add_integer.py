7b99a7435d19aedc90d4a0a942a4eb6c0335f6f2
Stuart Kuredjian <108@holbertonschool.com>
2017-05-17 01:50:47 +0000

    cleanup

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ args passed must be of type int or float otherwise TypeError is raised
 float args are cast as int
 """
 
+
 def add_integer(a, b):
     """
     add_integer:
@@ -12,8 +13,8 @@ def add_integer(a, b):
     returns int cassted sum of args
     """
 
-    if not isinstance(a, int)  and not isinstance(a, float):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int)  and not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
