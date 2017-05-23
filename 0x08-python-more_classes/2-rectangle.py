9d4cb273ef7c004ce571bf7a27ce35b1a84e5099
Stuart Kuredjian <108@holbertonschool.com>
2017-05-23 20:17:16 +0000

    2-rectangle.py: initial commit

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+"""
+Module 2-rectangle
+provides several methods pertainning to the size of a rectangle
+"""
+
+
+class Rectangle:
+    """
+    defines a rectangle by:
+      - private instance attributes 'width' and 'height'
+      - instantiation with optional 'width' and 'height'
+      - public instance method 'area'
+      - public instance method 'perimeter'
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        initializes Rectangle with optional width and height
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        width getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        width setter
+        """
+        self.handle_width_errors(value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        height getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        height setter
+        """
+        self.handle_height_errors(value)
+        self.__height = value
+
+    def area(self):
+        """
+        returns the area of a rectangle instance
+        """
+        self.handle_width_errors(self.__width)
+        self.handle_height_errors(self.__height)
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        returns the perimter of a rectangle instance
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def handle_width_errors(self, width):
+        """
+        raises an error if width isn't a positive integer
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+    def handle_height_errors(self, height):
+        """
+        raises an error if height isn't a positive integer
+        """
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
