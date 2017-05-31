66e8e729bcdde0117a894c916b38c0c9c97add9d
Stuart Kuredjian <108@holbertonschool.com>
2017-05-31 00:55:33 +0000

    8-rectangle.py: initial commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module:  8-rectangle.py
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    inherits from BaseGeometry
+      - `width` and `height` must be private
+      - `width` and `height` must be positive integers validated by
+        `integer_validator`
+    """
+    def __init__(self, width=0, height=0):
+        """
+        uses the superclass method, integer_validtaor() to
+        confirm 'width' and 'height' are both positive integers
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
