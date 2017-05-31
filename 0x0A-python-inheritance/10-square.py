ccb1486c2408ac0c79277b7f1f5c2b8bdb9370c4
Stuart Kuredjian <108@holbertonschool.com>
2017-05-31 02:22:48 +0000

    10-square.py: initial commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+"""
+Module:  10-square.py
+"""
+
+
+class Square(Rectangle):
+    """
+    inherits from `Rectangle`, which inherits from BaseGeometry
+    """
+    def __init__(self, size):
+        """
+        initializes Square and makes Rectangle's public attributes accessbile
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
