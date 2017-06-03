aeda2809a9310965b165319262b9cfbd2ca70a7a
Stuart <sgkur04@gmail.com>
2017-06-03 23:01:36 +0000

    fixing possible indent error due to emacs indent bug

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -8,7 +8,9 @@ def write_file(filename="", text=""):
     """
     writes a string to a text file and returns the number of characters written
     """
+    chars = 0
+
     with open("my_first_file.txt", mode="w", encoding="utf-8") as _file:
         chars = _file.write(text)
 
-    return chars
+        return chars
