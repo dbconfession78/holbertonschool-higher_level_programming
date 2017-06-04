0db9ac6e1085e20265daa9ebe5f389eea0b06553
Stuart <sgkur04@gmail.com>
2017-06-04 00:05:54 +0000

    PASSED: trying with try instead of if conditional

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -12,8 +12,11 @@ load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 _list = []
 
-if os.path.exists("add_item.json"):
+# if os.path.exists("add_item.json"):
+try:
     _list = load_from_json_file("add_item.json")
+except:
+    pass
 
 for i in range(1, len(sys.argv)):
     _list.append(sys.argv[i])
