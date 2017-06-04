e743228e083e35c400dcd67a9509642ca29709b7
Stuart <sgkur04@gmail.com>
2017-06-04 00:10:28 +0000

    PASSED: remove 'import os'

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,7 +6,6 @@ adds all arguments to a Python list, and then saves them to a file
 
 
 import sys
-import os
 
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
