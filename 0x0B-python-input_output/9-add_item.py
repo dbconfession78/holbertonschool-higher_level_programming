8f72453411b137a9d74bcd16934aa36c64906275
Stuart <sgkur04@gmail.com>
2017-06-04 00:00:06 +0000

    9-add_item.py: initial commit

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module: 9-add_item
+adds all arguments to a Python list, and then saves them to a file
+"""
+
+
+import sys
+import os
+
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+_list = []
+
+if os.path.exists("add_item.json"):
+    _list = load_from_json_file("add_item.json")
+
+for i in range(1, len(sys.argv)):
+    _list.append(sys.argv[i])
+
+save_to_json_file(_list, "add_item.json")
