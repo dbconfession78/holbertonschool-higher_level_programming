f98bba2dcc6e8d20aff691c00303e169cf9f2e97
Stuart <sgkur04@gmail.com>
2017-06-04 00:11:03 +0000

    remove commented if condition

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -11,7 +11,6 @@ load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 _list = []
 
-# if os.path.exists("add_item.json"):
 try:
     _list = load_from_json_file("add_item.json")
 except:
