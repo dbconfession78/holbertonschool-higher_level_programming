f1f60f39ef4dfda968f776f64734b16bb4c52a92
Stuart Kuredjian <108@holbertonschool.com>
2017-07-13 19:59:08 +0000

    2-create_read_user.sql: initial commit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS  `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY
+'ser_0d_2_pwd';
+REVOKE ALL PRIVILEGES ON * . * FROM 'user_0d_2'@'localhost';
+GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
