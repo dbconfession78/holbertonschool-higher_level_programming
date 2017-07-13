c02dba8243eb8f8bfac5ce6db1abb1a11243ffdc
Stuart Kuredjian <108@holbertonschool.com>
2017-07-13 19:09:55 +0000

    9-full_creation.sql: remove line breaks from server copy

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,6 @@
 -- creates a table 'second_table' in the selected db and adds multiple rows
-CREATE TABLE IF NOT EXISTS second_table (id INT, name V\
-ARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) values (1, '\
-John', 10);
-INSERT INTO second_table (id, name, score) values (2, '\
-Alex', 3);
-INSERT INTO second_table (id, name, score) values (3, '\
-Bob', 14);
-INSERT INTO second_table (id, name, score) values (4, '\
-George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) values (1, 'John', 10);
+INSERT INTO second_table (id, name, score) values (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) values (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) values (4, 'George', 8);
