cca28fd0acf842ee9e044ab599eea12e3a81c061
Stuart Kuredjian <108@holbertonschool.com>
2017-07-13 19:30:46 +0000

    11-best_score.sql: add space after -- in description comment

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
---lists all records with a score >= 10 in 'second_table' of the selected db
-SELECT `score`, `name` FROM `second_table` WHERE score >= 10 ORDER BY `score` DESC;
+-- lists all records with a score >= 10 in 'second_table' of the selected db
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
