2ba13f62c3dca5f1e56b3ecb9892970d7cffe69b
Stuart Kuredjian <108@holbertonschool.com>
2017-07-15 23:41:23 +0000

    13-count_shows_by_genre.sql :cleanup format

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 SELECT tv_genres.`name` as 'genre', COUNT(tv_show_genres.genre_id) AS 'numbe\
 r_shows'
 FROM tv_genres
-INNER JOIN `tv_show_genres` ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.`name`
 ORDER BY 2 DESC;
