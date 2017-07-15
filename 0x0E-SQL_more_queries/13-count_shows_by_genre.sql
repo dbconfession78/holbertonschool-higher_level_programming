f35aab465e6ca91301b14837b43f77f55bcdaccb
Stuart Kuredjian <108@holbertonschool.com>
2017-07-15 23:35:22 +0000

    13-count_shows_by_genre.sql :initial commit

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all genres from selected db and displays the number of shows linked to each
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY 2 DESC;
