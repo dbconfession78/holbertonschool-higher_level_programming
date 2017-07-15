fe2c4a2352fee9e9a75834e28466a1da46a9a706
Stuart Kuredjian <108@holbertonschool.com>
2017-07-15 23:53:04 +0000

    13-count_shows_by_genre.sql :use tv_show_genres for FROM statement instead of tv_genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- lists all genres from selected db and displays the number of shows linked to each
 SELECT tv_genres.`name` AS 'genre', COUNT(tv_show_genres.genre_id) AS 'numbe\
 r_shows'
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+FROM tv_show_genres
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.`name`
 ORDER BY 2 DESC;
