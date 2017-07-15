4720f66cb7a65c3159233901b065fad3ea27a3a7
Stuart Kuredjian <108@holbertonschool.com>
2017-07-15 22:21:28 +0000

    12-no_genre.sql :initial commit

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in selected db without a genre linked
+-- each record displays tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
