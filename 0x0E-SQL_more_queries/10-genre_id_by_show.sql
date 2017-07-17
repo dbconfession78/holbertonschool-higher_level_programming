6f92c8f5396c38e32bc380efd5cae431562900d2
Stuart Kuredjian <108@holbertonschool.com>
2017-07-17 20:48:16 +0000

    10-genre_id_by_show.sql :refactored to use 'JOIN' instead of 'WHERE'

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,11 @@
 -- lists all shows contained in selected db
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.show_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
+-- This can also be done with 'WHERE' instead of 'JOIN'
+-- SELECT tv_shows.title, tv_show_genres.genre_id
+-- FROM tv_shows, tv_show_genres
+-- WHERE tv_shows.id = tv_show_genres.show_id
+-- ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
