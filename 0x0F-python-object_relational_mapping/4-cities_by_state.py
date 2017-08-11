70fe0e025816928799fb7e0d72444cc5bd78adca
Stuart <sgkur04@gmail.com>
2017-08-11 03:33:29 +0000

    4-cities_by_state.py :initial commit

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" injection-safe script that takes an state name as arg and lists
+all cities of that state  """
+
+
+def main():
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+#    state_arg = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=passwd,
+        db=db_name)
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name \
+        FROM cities \
+        JOIN states ON cities.state_id = states.id \
+        ORDER BY cities.id ASC")
+
+    city = cur.fetchone()
+    while (city):
+        print(city)
+        city = cur.fetchone()
+
+if __name__ == "__main__":
+    main()
