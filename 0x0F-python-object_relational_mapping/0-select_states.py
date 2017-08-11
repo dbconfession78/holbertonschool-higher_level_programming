7564f1a095ddd92ffd72f38b37eef23803263909
Stuart <sgkur04@gmail.com>
2017-08-11 01:02:42 +0000

    0-select_states.py :PASSED refactoring to how I had it before discovered shebang was wrong

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,25 +2,28 @@
 """ lists all the states from the db hbtn_0e_0_usa """
 
 
-if __name__ == "__main__":
+def main():
     import MySQLdb
     import sys
 
     username = sys.argv[1]
-    passwd = sys.argv[2]
+    password = sys.argv[2]
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        password=passwd,
-        db=db_name)
+        user="{}".format(username),
+        password="{}".format(password),
+        db="{}".format(db_name))
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     line = cur.fetchone()
     while (line):
         print(line)
         line = cur.fetchone()
+
+if __name__ == "__main__":
+    main()
