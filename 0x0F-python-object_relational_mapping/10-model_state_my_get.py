6a884c305413aa4a1cae3aa882cdf6e1b3222a22
Stuart Kuredjian <108@holbertonschool.com>
2017-08-12 02:13:58 +0000

    10-model_state_my_get.py :revert to previous ver after fixing uppercase error

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,9 +2,10 @@
 """
 prints the state's id
 """
+import MySQLdb
 from sys import argv
+from sqlalchemy import create_engine
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
@@ -14,19 +15,18 @@ def main():
     db_name = argv[3]
     state_arg = argv[4]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
-                           format(username, passwd, db_name))
-    Base.metadata.create_all(engine)
-
+    engine = create_engine("mysql://{}:{}@localhost/{}".format(
+        username, passwd, db_name))
+#    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    try:
-        state = session.query(State).filter(State.name == state_arg).one()
-        print("{}".format(state.id))
-    except:
+    state = session.query(State.id).filter(
+         State.name.like("%{}%".format(state_arg)))
+    if state.first() is None:
         print("Not found")
-    session.close()
+    else:
+        print(state[0].id)
 
 if __name__ == "__main__":
     main()
