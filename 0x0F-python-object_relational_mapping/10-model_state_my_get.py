ced05c715deda3f8991013c890289719189e0a8f
Stuart Kuredjian <108@holbertonschool.com>
2017-08-12 01:10:52 +0000

    10-model_state_my_get.py :revert

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-# Lists all states from a database
+"""
+prints the state's id
+"""
+import MySQLdb
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    import sqlalchemy
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
-    from sys import argv, exit
 
-    if len(argv) != 5:
-        print("Usage: ./10.py <usrname> <passwd> <database> <search>")
-        exit(1)
+def main():
+    username = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    state_arg = argv[4]
 
-    usr, pwd, dbe = argv[1], argv[2], argv[3]
-    sch = argv[4].split("'")[0]
+    engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(
+        username, passwd, db_name))
 
-    eng = "mysql://" + usr + ":" + pwd + "@localhost:3306/" + dbe
-    try:
-        engine = sqlalchemy.create_engine(eng)
-    except Exception as err:
-        print(err)
-        exit(1)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    query = session.query(State).filter_by(name=sch)
+    query = session.query(State.id).filter_by(name=state_arg)
     if query.first() is None:
         print("Not Found")
     else:
         print("{:d}".format(query[0].id))
     session.close()
+
+
+if __name__ == "__main__":
+    main()
