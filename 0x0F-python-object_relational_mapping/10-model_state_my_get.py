add0456d1d755e2294f5471b53e918f25d37d301
Stuart Kuredjian <108@holbertonschool.com>
2017-08-12 02:05:23 +0000

    10-model_state_my_get.py :revert

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-'''
-prints the State object with the name passed as argument
-'''
-import sys
+"""
+prints the state's id
+"""
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    search_for = sys.argv[4]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
 
+def main():
+    username = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    state_arg = argv[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(username, passwd, db_name))
+
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    '''
-    for state in session.query(State).filter(State.name == search_for):
-        if state:
-            print("{}".format(state.id))
-    '''
+
     try:
-        state = session.query(State).filter(State.name == search_for).one()
+        state = session.query(State).filter(State.name == state_arg).one()
         print("{}".format(state.id))
     except:
-        print("Not found")
+        print("Not Found")
     session.close()
+
+if __name__ == "__main__":
+    main()
