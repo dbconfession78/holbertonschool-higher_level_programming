06107e8bd83af99e04f0338ad61844abedd2071d
Stuart <sgkur04@gmail.com>
2017-08-22 23:10:51 +0000

    0-body_size.sh :try with awk instead of grep

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # displays the value of the 'Content-Length' response header
-curl -sI "$1" | grep Content-Length | cut -d' ' -f2
-
+# curl -sI "$1" | grep Content-Length | cut -d' ' -f2
+curl -si "$1" |awk '$1 ~ /Content-Length/ {print $2}'
 # This will get character count of curl response. Not the most accurate, but
 # useful if their is no Content-Length header
 
