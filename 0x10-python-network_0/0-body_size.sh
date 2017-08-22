7fa8bbbe830ecd963e03b7a429f40f93f2d898ed
Stuart <sgkur04@gmail.com>
2017-08-22 23:16:43 +0000

    0-body_size.sh :remove comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # displays the value of the 'Content-Length' response header
 curl -sI "$1" | grep Content-Length | cut -d' ' -f2
-
-
-# curl -si "$1" |awk '$1 ~ /Content-Length/ {print $2}'
-# This will get character count of curl response. Not the most accurate, but
-# useful if their is no Content-Length header
-
-# response=$(curl -s $1)
-# wc=$(echo $response | wc -c)
-# echo $wc
