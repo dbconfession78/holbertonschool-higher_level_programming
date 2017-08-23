ef10fd8de6b888b1084aa56362e86c65f149a8d1
Stuart <sgkur04@gmail.com>
2017-08-23 00:28:57 +0000

    5-post_params.sh :specify that it is a POST instead of just including data

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the response body of a POST request
-curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s "$1" -X "POST" --data "email=hr@holbertonschool.com&subject=I will always be here for PLD
