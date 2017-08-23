72d59fc66e8b2e265e29ba0b911f12ec5c91efcf
Stuart <sgkur04@gmail.com>
2017-08-23 00:32:55 +0000

    5-post_params.sh :make executable

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the response body of a POST request
-curl -s "$1" -X "POST" --data "email=hr@holbertonschool.com&subject=I will always be here for PLD
+curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
