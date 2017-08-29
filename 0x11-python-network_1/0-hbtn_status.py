b7b9ac2fad3638f819f0e1ae75b78064ce6cad49
Stuart <sgkur04@gmail.com>
2017-08-29 09:59:42 +0000

    0-hbtn_status.py :change 'utf' to 'utf8'

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,7 +14,7 @@ def main():
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- utf content: {}'.format(html.decode('utf-8')))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
 
 if __name__ == '__main__':
     main()
