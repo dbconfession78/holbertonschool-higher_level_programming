c129578b54497432048211b705c9c72bcaed619a
Stuart Kuredjian <108@holbertonschool.com>
2017-08-29 00:38:57 +0000

    101-starwars.py initial commit

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+takes in a string and sends a search request to the Star Wars API
+"""
+import requests
+from sys import argv
+
+query = argv[1]
+next = 'https://swapi.co/api/people/?search={}'.format(query)
+names = []
+while (next):
+    json = requests.get(next).json()
+    results = json.get('results')
+    for result in results:
+        names.append(result['name'])
+
+    try:
+        next = json['next']
+    except:
+        next = None
+
+print('Number of results: {}'.format(json.get('count')))
+for name in names:
+    print(name)
