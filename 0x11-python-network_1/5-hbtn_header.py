09ae0f40b31a57aeff0dd0ab56148db1e6d28885
Stuart <sgkur04@gmail.com>
2017-08-29 09:56:09 +0000

    5-hbtn_header.py :use r.headers.get('X-Request-Id') instead of r.headers['X-Request-Id']

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -13,7 +13,7 @@ def main():
     url = argv[1]
     r = requests.get(url)
 
-    print(r.headers['X-Request-Id'])
+    print(r.headers.get('X-Request-Id'))
 
 if __name__ == '__main__':
     main()
