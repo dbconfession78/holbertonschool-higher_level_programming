bb9cc181c745fcefcab9c4316661f3fc024f2ff6
Stuart <sgkur04@gmail.com>
2017-09-19 22:39:23 +0000

    2-arguments.js :remove use of .slice(2)

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length == 0) {
+const args = process.argv;
+if (args.length <= 2) {
 	console.log('No argument');
-} else if (args.length == 1) {
+} else if (args.length == 3) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
