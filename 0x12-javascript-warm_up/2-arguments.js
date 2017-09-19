d6ba9a540b825da7c21157597f764c634d0c87ab
Stuart <sgkur04@gmail.com>
2017-09-19 22:58:07 +0000

    2-arguments.js : revert and chnage tab indents to double spaces

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-// prints a message depending on number of arguments passed
-if (process.argv.length === 2) {
+// outputs if an argument, arguments or no arguments were passed to this script
+const args = process.argv;
+if (args.length <= 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (args.length == 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
