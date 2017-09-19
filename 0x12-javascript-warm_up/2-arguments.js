b7367597a4a06ba42ee2498c4ceab04af6a8094e
Stuart <sgkur04@gmail.com>
2017-09-19 22:49:23 +0000

    2-arguments.js :add documentation

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// outputs if an argument, arguments or no arguments were passed to this script
 if (process.argv.length === 2) {
 	console.log('No argument');
 } else if (process.argv.length === 3) {
