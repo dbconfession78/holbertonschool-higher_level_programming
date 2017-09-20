a554c5734e8003cdcbe0f0805ddf84c74825e9ed
Stuart <sgkur04@gmail.com>
2017-09-20 00:35:39 +0000

    8-square.js : use .repeat instead of nested loop with process.stdout.write()

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,9 +5,6 @@ if (isNaN(args[0])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-		process.stdout.write('X');
-    }
-	  console.log();
+    console.log('X'.repeat(n))
   }
 }
