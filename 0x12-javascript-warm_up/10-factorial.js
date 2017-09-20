dc8bd0f6647d880573a2319b09e59c0087a9a5ef
Stuart Kuredjian <108@holbertonschool.com>
2017-09-20 02:01:55 +0000

    10-factorial.js :parse argument as Int

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const num = process.argv.slice(2)[0];
+const num = parseInt(process.argv.slice(2)[0]);
 function fact(n) {
   if (isNaN(n) || (n === 0)) {
     return 1;
   }
   return n * fact(n - 1);
 }
-
 console.log(fact(num));
