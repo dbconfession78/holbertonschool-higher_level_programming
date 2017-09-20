3165fd26208159b6b334febc51e6008b1629e5ef
Stuart <sgkur04@gmail.com>
2017-09-20 00:33:00 +0000

    8-square.js :initial commit

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const n = args[0];
+if (isNaN(args[0])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+		process.stdout.write('X');
+    }
+	  console.log();
+  }
+}
