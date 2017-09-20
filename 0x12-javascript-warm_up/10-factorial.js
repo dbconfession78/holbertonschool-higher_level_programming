af126eaa7f57249bf4577afda61ec6a0f3fb3afd
Stuart Kuredjian <108@holbertonschool.com>
2017-09-20 01:42:56 +0000

    10-factorial.js : initial commit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+num = args[0];
+
+let fact = function(n) {
+  if (n == 0 || isNaN(n)) {
+    return (1);
+  }
+  return fact(n - 1) * n;
+}
+
+/*
+function fact(n) {
+  if (n == 0 || isNaN(n)) {
+    return (1);
+  }
+  return fact(n - 1) * n;
+}
+*/
+
+console.log(fact(num));
