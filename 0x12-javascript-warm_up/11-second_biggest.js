2d798afe09e64d7f05e916d5e9d4f9954a80b9e2
Stuart Kuredjian <108@holbertonschool.com>
2017-09-20 02:32:01 +0000

    11-second_biggest.js :initial commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const num = args[0];
+let list = [];
+for (let i = 0; i < args.length; i++) {
+  list[i] = parseInt(args[i]);
+}
+
+let sec_biggest = function(list) {
+  if (list.length <= 1) {
+    return (1);
+  }
+  let max = Math.max.apply(Math, list);
+  let newList = [];
+  let x = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] != max) {
+      newList[x] = list[i];
+      x++;
+    }  
+  }
+  return Math.max.apply(Math, newList);
+}
+const sec_high = sec_biggest(list);
+console.log(sec_high);
