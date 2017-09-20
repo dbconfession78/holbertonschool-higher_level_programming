4eb1346144b56da790d30befaa7510fe33bfae32
Stuart <sgkur04@gmail.com>
2017-09-20 00:43:33 +0000

    8-square.js : revert and clean up indents

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,6 +5,9 @@ if (isNaN(args[0])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < n; i++) {
-    console.log('X'.repeat(n))
+    for (let j = 0; j < n; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
   }
 }
