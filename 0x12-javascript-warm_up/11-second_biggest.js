a697bdaa9a7529cd78a53019716f2eeea58ca804
Stuart Kuredjian <108@holbertonschool.com>
2017-09-20 02:52:01 +0000

    m : use sort and pop to get last two

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const num = args[0];
-let list = [];
-for (let i = 0; i < args.length; i++) {
-  list[i] = parseInt(args[i]);
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  args.sort((a, b) => a - b);
+  args.pop();
+  console.log(args.pop());
 }
-
-let sec_biggest = function(list) {
-  if (list.length <= 1) {
-    return (0);
-  }
-  let max = Math.max.apply(Math, list);
-  let newList = [];
-  let x = 0;
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] != max) {
-      newList[x] = list[i];
-      x++;
-    }  
-  }
-  return Math.max.apply(Math, newList);
-}
-const sec_high = sec_biggest(list);
-console.log(sec_high);
