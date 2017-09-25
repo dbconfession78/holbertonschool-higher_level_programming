e619c530bb3109921a09262a70bc6bdef2e6f9d6
Stuart Kuredjian <108@holbertonschool.com>
2017-09-25 23:45:33 +0000

    7-occurrences.js : add keyword let to initial retval

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // counts the number of occurances in a list
 exports.nbOccurences = function (list, search_element) {
-  retval = 0;
+  let retval = 0;
   for (let i = 0; i < list.length; i += 1) {
     if (list[i] == search_element) {
       retval += 1;
