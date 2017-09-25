3f0b5f5aecfe32a45c45bb61601cb9ba813d01f3
Stuart Kuredjian <108@holbertonschool.com>
2017-09-25 23:29:54 +0000

    7-occurrences.js :initial

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// counts the number of occurances in a list
+exports.nbOccurences = function (list, search_element) {
+  retval = 0;
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i] == search_element) {
+      retval += 1;
+    }
+  }
+  return retval;
+};
