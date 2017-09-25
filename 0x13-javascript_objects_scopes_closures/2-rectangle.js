f2487ac1a7b7ea871d97ed8571c4447504e56205
Stuart Kuredjian <108@holbertonschool.com>
2017-09-25 19:48:02 +0000

    2-rectangle.js: revert

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 // a Rectangle constructor with conditional width and height
-function Rectangle(w,h) {
+exports.Rectangle = function Rectangle (w, h) {
   if (w > 0 && h > 0) {
     this.width = w;
     this.height = h;
   }
-}
-
-exports.Rectangle = Rectangle;
+};
