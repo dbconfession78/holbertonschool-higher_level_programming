f53d7dcca2e03b33bd0e04e6e43e7eeda0acd11e
Stuart Kuredjian <108@holbertonschool.com>
2017-09-25 19:46:32 +0000

    2-rectangle.js :PASSED: testing diff format

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 // a Rectangle constructor with conditional width and height
-exports.Rectangle = function Rectangle (w, h) {
+function Rectangle(w,h) {
   if (w > 0 && h > 0) {
     this.width = w;
     this.height = h;
   }
-};
+}
+
+exports.Rectangle = Rectangle;
