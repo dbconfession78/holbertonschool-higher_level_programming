3c0bb7ea211cdeeb36ab4c9f37c2b90730235f0f
Stuart Kuredjian <108@holbertonschool.com>
2017-09-25 21:49:31 +0000

    5-square.js :add space and semicolon

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,6 +2,6 @@
 // the Square class setup to niherit from the rectangle class
 const Rectangle = require('./4-rectangle').Rectangle;
 
-exports.Square = function Square(size) {
-  Rectangle.call(this, size, size)
+exports.Square = function Square (size) {
+  Rectangle.call(this, size, size);
 }
