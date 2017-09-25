62ce14fa6e45ee40bcda66f1859ebb535a620d17
Stuart Kuredjian <108@holbertonschool.com>
2017-09-25 22:06:22 +0000

    5-square.js :remove block comment

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 // the Square class setup to niherit from the rectangle class
 const Rectangle = require('./4-rectangle').Rectangle;
-/*
-exports.Square = function Square (size) {
-  Rectangle.call(this, size, size);
-};
-*/
 
 function Square (size) {
   Rectangle.call(this, size, size);
 }
 
 exports.Square = Square;
-
