10591caa3b119edd9c7bc523087c3eecfb351f21
Stuart Kuredjian <108@holbertonschool.com>
2017-09-25 19:39:16 +0000

    1-rectangle.js :refactor

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // defines the Rectangle constructor
 exports.Rectangle = function Rectangle (w, h) {
-	this.width = w;
-	this.height = h;
+  this.width = w;
+  this.height = h;
 };
-
