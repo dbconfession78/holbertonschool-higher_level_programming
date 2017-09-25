acd40c5b9ec5717329164f3a1ec5bf0d00598423
Stuart Kuredjian <108@holbertonschool.com>
2017-09-25 21:53:59 +0000

    5-square.js :add space

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,7 @@
 // the Square class setup to niherit from the rectangle class
 const Rectangle = require('./4-rectangle').Rectangle;
 
-function Square(size) {
+function Square (size) {
   Rectangle.call(this, size, size);
 }
 
