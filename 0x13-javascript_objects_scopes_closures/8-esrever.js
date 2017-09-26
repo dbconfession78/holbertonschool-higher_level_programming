1772574f2da2eba961adbd8ed8343f3111c8ea20
Stuart Kuredjian <108@holbertonschool.com>
2017-09-26 00:01:00 +0000

    8-esrever.js :add semicolon to end

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+// returns a reversed list based on the given list
 exports.esrever = function (list) {
   let retval = [list.length];
   for (let i=0; i < list.length; i += 1) {
     retval[i] = list[list.length-i-1];
   }
   return (retval);
-}
+};
 
