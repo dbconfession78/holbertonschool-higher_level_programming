d9a3ce0fc6061bb7839f4eabe76093c5a03f3c20
Stuart Kuredjian <108@holbertonschool.com>
2017-09-26 00:46:49 +0000

    8-esrever.js :use push intead of index

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // returns a reversed list based on the given list
 exports.esrever = function (list) {
-  let retval = [list.length];
-  for (let i=0; i < list.length; i += 1) {
-    retval[i] = list[list.length-i-1];
+  let retval = [];
+  for (let i = 0; i < list.length; i++) {
+    retval.push(list[list.length-i-1]);
   }
-  return (retval);
+  return retval;
 };
-
