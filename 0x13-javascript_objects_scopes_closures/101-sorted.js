f9ceeb1b053835da61c933b38a3549e684a7aeed
Stuart Kuredjian <108@holbertonschool.com>
2017-09-26 03:58:12 +0000

    101-sorted.js :PASSED - trying original solution

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,34 @@
 #!/usr/bin/node
-const oDict = require('./101-data').dict;
-let fDict = {};
+const oDict = require('./101-dict').dict;
+let list = [];
+let retval = {};
 
 for (let key in oDict) {
-  const count = oDict[key];
-  if (!(count in fDict)) {
-    fDict[count] = [key];
-  } else {
-    fDict[count].push(key);
+  const val = oDict[key];
+  if (contains(list, val) === false) {
+    list.push(val);
   }
 }
-console.log(fDict);
+list.sort();
+
+for (let i in list) {
+  let idList = [];
+  for (let key in oDict) {
+    const val = oDict[key];
+    if (val === list[i]) {
+      idList.push(key);
+    }
+  }
+  retval[list[i]] = idList;
+}
+console.log(retval);
+
+
+function contains (list, n) {
+  for (let i in list) {
+    if (list[i] === n) {
+      return true;
+    }
+  }
+  return false;
+}
