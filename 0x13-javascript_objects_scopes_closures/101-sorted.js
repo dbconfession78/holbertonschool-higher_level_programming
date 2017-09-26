d374a5211613d8f87878d0ad03de3898881243d3
Stuart Kuredjian <108@holbertonschool.com>
2017-09-26 02:43:40 +0000

    101-sorted.js :initial

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+const oDict = require('./101-main').dict;
+let list = [];
+retval = {};
+
+for (key in oDict) {
+  const val = oDict[key]
+  if (contains(list, val) === false) {
+    list.push(val);
+  }
+}
+
+for (i in list) {
+  idList = []
+  for (key in oDict) {
+    const val = oDict[key];
+    if (val === list[i]) {
+      idList.push(key);
+    }
+  }
+  retval[list[i]] = idList;
+}
+console.log(retval);
+
+
+function contains (list, n) {
+  for (i in list) {
+    if (list[i] === n) {
+      return true;
+    }
+  }
+  return false;
+}
