96dd8132c87cbf72a48afb6554e21636a2a99ea9
Stuart Kuredjian <108@holbertonschool.com>
2017-09-26 00:03:47 +0000

    7-occurrences.js :use === instead of ==

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,9 +3,9 @@
 exports.nbOccurences = function (list, search_element) {
   let retval = 0;
   for (let i = 0; i < list.length; i += 1) {
-    if (list[i] == search_element) {
+    if (list[i] === search_element) {
       retval += 1;
     }
   }
-  return retval;
+  return (retval);
 };
