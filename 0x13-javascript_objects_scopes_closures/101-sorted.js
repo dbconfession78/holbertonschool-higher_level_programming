938081eca4d9062f5c535be33b274ae1ca3b5c42
Stuart Kuredjian <108@holbertonschool.com>
2017-09-26 02:55:23 +0000

    101-sorted.js :add missing 'let's and semicolons

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
 const oDict = require('./101-dict').dict;
 let list = [];
-retval = {};
+let retval = {};
 
-for (key in oDict) {
-  const val = oDict[key]
+for (let key in oDict) {
+  const val = oDict[key];
   if (contains(list, val) === false) {
     list.push(val);
   }
 }
 
-for (i in list) {
-  idList = []
-  for (key in oDict) {
+
+for (let i in list) {
+  let idList = [];
+  for (let key in oDict) {
     const val = oDict[key];
     if (val === list[i]) {
       idList.push(key);
@@ -24,7 +25,7 @@ console.log(retval);
 
 
 function contains (list, n) {
-  for (i in list) {
+  for (let i in list) {
     if (list[i] === n) {
       return true;
     }
