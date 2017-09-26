94ca037999c72c476fddcde87392896cb27e9058
Stuart Kuredjian <108@holbertonschool.com>
2017-09-26 00:56:47 +0000

    8-esrever.js :remove whitespace

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,8 @@
 // returns a reversed list based on the given list
 exports.esrever = function (list) {
   let retval = [];
-  for (let i  = list.length - 1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
+//  for (let i = 0; i < list.length; i++) {
     retval.push(list[i]);
   }
   return retval;
